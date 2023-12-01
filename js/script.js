81b82b517708f0efde485460b33c257b4d00a6cd
KryvaSofia <119862637+SofiaK031@users.noreply.github.com>
2023-12-01 10:02:00 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    $("#btn1").on("click", function () {
+        window.open("https://sofiak031.github.io/Frontend_LR7/", '_blank');
+    });
+
+    $("#btn2").on("click", function () {
+        window.open("https://sofiak031.github.io/Frontend_LR9/", '_blank');
+    });
+
+    $("#btn3").on("click", function () {
+        window.open("https://sofiak031.github.io/Frontend_LR10/", '_blank');
+    });
+
+    $("#btn4").on("click", function () {
+        window.open("https://sofiak031.github.io/Frontend_LR12/", '_blank');
+    });
+});
